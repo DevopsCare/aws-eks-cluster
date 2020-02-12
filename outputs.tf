74360372c5f732ca09e55a9359dabbc985faea34
vkropotko <vjkropotko@gmail.com>
2020-02-12 18:17:12 +0200

    Update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "kubeconfig_filename" {
-  value = module.eks.kubeconfig_filename
-}
-
 output "whitelist_sg_id" {
   value = aws_security_group.whitelist.id
 }
@@ -18,3 +14,18 @@ output "eks_cluster" {
   value = module.eks
 }
 
+output "kubeconfig_filename" {
+  value = module.eks.kubeconfig_filename
+}
+
+output "kubernetes_host" {
+  value = data.aws_eks_cluster.cluster.endpoint
+}
+
+output "kubernetes_ca_certificate" {
+  value = data.aws_eks_cluster.cluster.certificate_authority.0.data
+}
+
+output "kubernetes_token" {
+  value = data.aws_eks_cluster_auth.cluster.token
+}
