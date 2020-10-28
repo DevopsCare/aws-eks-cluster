16b862ec818c0d6a1904be43a3edc865398f3e89
Pavels Veretennikovs <PVeretennikovs@riskfocus.com>
2020-10-28 19:46:45 +0200

    feat: bump Kubernetes to 1.13, helm to 1.3

--- main.tf
+++ main.tf
@@ -23,11 +23,11 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 provider "kubernetes" {
+  version                = "~>1.13"
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
-  version                = "~> 1.9"
 }
 
 data "aws_caller_identity" "current" {}
